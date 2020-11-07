381c75ea9b44707c765761d8a671909222a43b5f
Rippan <jepperipnas@gmail.com>
2020-11-07 04:49:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DLTP_Phase1_AdressBook2
+{
+    class Program
+    {
+        List<Person> Book = new List<Person>();
+        static void Main(string[] args)
+        {
+            
+        }
+        class Person
+        {
+            public string name, adress, phonenumber, email;
+            public Person(string N, string A, string P, string E)
+            {
+                name = N;
+                adress = A;
+                phonenumber = P;
+                email = E;
+            }
+        }
+    }
+}
