4f39443539c0135f8830b44e465d33dcb207c40b
Rippan <jepperipnas@gmail.com>
2020-11-07 05:17:19 +0100

    Added method to read file & fill list with data

--- Program.cs
+++ Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
+using System.Data;
 
 namespace DLTP_Phase1_AdressBook2
 {
     class Program
     {
-        List<Person> Book = new List<Person>();
+        private static List<Person> Book = new List<Person>(); // keep list outside methods, so every method can call for it
         static void Main(string[] args)
         {
+            string filePath = ReadFile();
+            bool quit = false;
             Menu();
+            ReadFile();
+            do
+            {
+                Console.ReadLine();
+            }
+            while (!quit);
         }
-        private static void Menu()
+        public static string ReadFile()
+        {
+            string filePath = @"C:\adressbook\contacts.txt";
+            string[] fileText = File.ReadAllLines(filePath);
+            //Read file and saves already written information into list
+            foreach (string row in fileText)
+            {
+                string[] split = row.Split(';');
+                Person N = new Person(split[0], split[1], split[2], split[3]);
+                Book.Add(N);
+            }
+            return filePath;
+        }
+        public static void Menu()
         {
             Console.WriteLine("**** Pick an option ****");
             Console.WriteLine("1: Show contacts");
@@ -20,7 +42,6 @@ namespace DLTP_Phase1_AdressBook2
             Console.WriteLine("4: Save to file");
             Console.WriteLine("End program by typing 'quit'");
             Console.Write("> ");
-            Console.ReadLine();
         }
         class Person
         {
